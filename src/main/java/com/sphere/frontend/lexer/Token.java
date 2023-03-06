9d748ea540bfd9133c940d1ecf10d0eae087a1f9
Vivallo04 <andresvivallo4@gmail.com>
2023-03-05 21:00:15 -0600

    Token constructor for character combinations

--- src/main/java/com/sphere/frontend/lexer/Token.java
+++ src/main/java/com/sphere/frontend/lexer/Token.java
@@ -5,6 +5,7 @@ import com.sphere.frontend.token.TokenType;
 public class Token {
 
     private char tokenText;
+    private String tokenString;
     private final TokenType type;
 
 
@@ -13,7 +14,23 @@ public class Token {
         this.type = tokenType;
     }
 
+    public Token(String tokenString, TokenType tokenType) {
+        this.tokenString = tokenString;
+        this.type = tokenType;
+    }
+
     public TokenType getType() {
         return type;
     }
+
+    public static TokenType checkIfKeyword(String tokenText) {
+        for (TokenType kind : TokenType.values()) {
+            // Relies on all keyword enum values being 1XX.
+            /* if (kind.name().equals(tokenText) && kind == "" && kind.value() < 200) {
+                return kind;
+            }*/
+        }
+        return null;
+    }
+
 }
