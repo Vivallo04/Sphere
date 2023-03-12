0d2cfab11134b81874404f71ca2173c8891d1878
Vivallo04 <andresvivallo4@gmail.com>
2023-03-11 23:59:07 -0600

    Parser interface implementation

--- src/main/java/com/sphere/frontend/parser/IParser.java
+++ src/main/java/com/sphere/frontend/parser/IParser.java
@@ -1,4 +1,14 @@
 package com.sphere.frontend.parser;
 
+import com.sphere.frontend.lexer.Token;
+
 public interface IParser {
+    boolean checkToken(Token kind);
+    boolean checkPeek(Token kind);
+    void match(Token kind);
+    void nextToken();
+    boolean isComparisonOperator();
+    void program();
+    void statement();
+    void comparison();
 }
