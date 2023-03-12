af33dbe868bdbd963a2dd3fbc0451bf12073c083
Vivallo04 <andresvivallo4@gmail.com>
2023-03-11 23:58:23 -0600

    Update to read the source as an input file

--- src/main/java/com/sphere/Main.java
+++ src/main/java/com/sphere/Main.java
@@ -1,19 +1,43 @@
 package com.sphere;
 
 import com.sphere.frontend.lexer.Lexer;
-import com.sphere.frontend.lexer.Token;
-import com.sphere.frontend.token.TokenType;
+import com.sphere.frontend.parser.Parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String source = "+-123 9.8654*/";
+        System.out.println("Crypton Compiler");
+        String source = "IF+-123 foo*THEN/";
+
+        readInputFile(args, source);
 
         Lexer lexer = new Lexer(source);
-        Token token = lexer.getToken();
+        Parser parser = new Parser(lexer);
 
-        while (token.getType() != TokenType.EOF) {
-            System.out.println(token.getType());
-            token = lexer.getToken();
+        parser.program();
+
+        System.out.println("Parsing completed");
+    }
+
+    private static void readInputFile(String[] args, String source) {
+        if (args.length != 1) {
+            System.out.println("Error: Compiler needs source file as argument.");
+            System.exit(1);
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                source += line + "\n";
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
