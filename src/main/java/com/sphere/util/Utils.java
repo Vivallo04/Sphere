ab2e2bda25c9fabb4f61adb16f6a61b9eafe7cbb
Vivallo04 <andresvivallo4@gmail.com>
2023-03-14 14:03:09 -0600

    Add methods to valid string types

--- src/main/java/com/sphere/util/Utils.java
+++ src/main/java/com/sphere/util/Utils.java
@@ -1,7 +1,15 @@
 package com.sphere.util;
 
 public class Utils {
-    public static boolean isAlphanumeric(char character) {
-        return Character.isDigit(character) || Character.isLetter(character);
+    public static boolean isAlphanumeric(String input) {
+        return input.matches("(?i)[a-z0-9]+");
+    }
+
+    public static boolean isDigit(String input) {
+        return input.matches("\\d*");
+    }
+
+    public static boolean isAlphabetic(String input) {
+        return input.matches("(?i)[a-zA-Z]+");
     }
 }
