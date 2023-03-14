13be4b49a13c163bc116104ec1f0436ce2652442
Vivallo04 <andresvivallo4@gmail.com>
2023-03-13 22:55:02 -0600

    Implement using emmiter

--- src/main/java/com/sphere/Main.java
+++ src/main/java/com/sphere/Main.java
@@ -1,5 +1,6 @@
 package com.sphere;
 
+import com.sphere.backend.Emitter;
 import com.sphere.frontend.lexer.Lexer;
 import com.sphere.frontend.parser.Parser;
 
@@ -11,36 +12,37 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Crypton Compiler");
-        String source = "IF+-123 foo*THEN/";
 
-        readInputFile(args, source);
+        String source = readInputFile(args);
 
         Lexer lexer = new Lexer(source);
-        Parser parser = new Parser(lexer);
+        Emitter emitter = new Emitter("out.c");
+        Parser parser = new Parser(lexer, emitter);
 
         parser.program();
+        emitter.writeFile();
 
-        System.out.println("Parsing completed");
+        System.out.println("Compiling completed");
     }
 
-    private static void readInputFile(String[] args, String source) {
+    private static String readInputFile(String[] args) {
         if (args.length != 1) {
-            System.out.println("Error: Compiler needs source file as argument.");
+            System.err.println("Error: Compiler needs source file as argument.");
             System.exit(1);
         }
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            StringBuilder sourceBuilder = new StringBuilder(source);
-            while ((line = reader.readLine()) != null) {
-                sourceBuilder.append(line).append("\n");
-            }
-            source = sourceBuilder.toString();
-            reader.close();
 
+        String filename = args[0];
+        StringBuilder source = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line = reader.readLine();
+            while (line != null) {
+                source.append(line);
+                line = reader.readLine();
+            }
         } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
+            System.err.println("Error reading file: " + e.getMessage());
             System.exit(1);
         }
+        return source.toString();
     }
 }
