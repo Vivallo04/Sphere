ede4b3e2ede73ce54bdca3e80f2af64cfa30320c
Vivallo04 <andresvivallo4@gmail.com>
2023-03-14 23:43:51 -0600

    Update build script to compile on Windows

--- build.ps1
+++ build.ps1
@@ -7,28 +7,3 @@ $SOURCE = "hello.crpt"
 
 # run the Main class
 & "$JAVA_HOME\bin\java" -cp "$PROJECT_DIR\build\classes\java\main; $PROJECT_DIR\build\resources\main; $PROJECT_DIR\build\libs\*" com.sphere.Main $SOURCE
-
-function compile {
-    $BN = [System.IO.Path]::GetFileNameWithoutExtension($args[0])
-    $TTOUTPUT = & $JAVA $COMPILER $args[0] 2>&1
-    if ($LASTEXITCODE -ne 0) {
-        Write-Output $TTOUTPUT
-    }
-    else {
-        Move-Item out.c "$BN.c"
-        $CCOUTPUT = & $CC -o $BN "$BN.c"
-        if ($LASTEXITCODE -ne 0) {
-            Write-Output $CCOUTPUT
-        }
-        else {
-            Write-Output $TTOUTPUT
-        }
-    }
-}
-
-if ($args.Length -eq 0) {
-    Get-ChildItem *.crpt | ForEach-Object { compile $_.FullName }
-}
-else {
-    compile $args[0]
-}
