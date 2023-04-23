10e339851f7c659cd2cb04939fd9eb68476ca0ad
Vivallo04 <andresvivallo4@gmail.com>
2023-04-22 20:06:07 -0600

    Update Main with ANTLR4 implementation

--- src/main/java/com/sphere/Main.java
+++ src/main/java/com/sphere/Main.java
@@ -1,36 +1,44 @@
 package com.sphere;
 
-import com.sphere.backend.Emitter;
-import com.sphere.frontend.lexer.Lexer;
-import com.sphere.frontend.parser.Parser;
+import com.sphere.backend.ArduinoCodeGenerator;
+import com.sphere.frontend.lexer.SphereLexer;
+import com.sphere.frontend.parser.SphereParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Compiler Started...\n");
+    public static void main(String @NotNull [] args) throws IOException {
+        System.out.println("Compiler started");
+        System.out.println("File: " + args[0] + "\n");
+        String input = readFile(args);
+    
+        var lexer = new SphereLexer(CharStreams.fromString(input));
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new SphereParser(tokens);
 
-        String source = readInputFile(args);
+        // Parse the input to create the parse tree
+        ParseTree tree = parser.start();
 
-        Lexer lexer = new Lexer(source);
-        Emitter emitter = new Emitter("src/main/resources/out.c");
-        Parser parser = new Parser(lexer, emitter);
+        System.out.println("Parse tree:");
+        System.out.println(tree.toStringTree(parser) + "\n");
 
-        parser.program();
-        emitter.writeFile();
+        // Walk the AST and generate Arduino code
+        var codeGenerator = new ArduinoCodeGenerator();
+        var arduinoCode = codeGenerator.visit(tree);
 
-
-        System.out.println("Compiling completed!");
+        codeGenerator.getWriter().close();
     }
 
-    private static String readInputFile(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Error: Compiler needs source file as argument.");
-            System.exit(1);
-        }
-
+    public static String readFile(@org.jetbrains.annotations.NotNull String[] args) {
         StringBuilder input = new StringBuilder();
         try {
             File file = new File(args[0]);
