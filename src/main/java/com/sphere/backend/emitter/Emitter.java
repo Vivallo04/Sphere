6fc133ec8d05764a6ba63ea6fcc4ee66a865c4d5
Vivallo04 <andresvivallo4@gmail.com>
2023-04-22 20:05:47 -0600

    Base emitter

---
+++ src/main/java/com/sphere/backend/emitter/Emitter.java
@@ -0,0 +1,7 @@
+package com.sphere.backend.emitter;
+
+import com.sphere.backend.emitter.interfaces.IEmitter;
+
+public class Emitter implements IEmitter {
+    // TODO: Implement emitter as the file writer of the Arduino code generator
+}
